c04c0eb8e40efc984bc22ca273093d752456ead1
Sachchida Nand Sharma <sachchidanand.s@srmtechsol.com>
2023-05-19 12:40:17 +0530

    Layout

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,14 +1,14 @@
 import {BrowserRouter,Routes, Route} from "react-router-dom"
-import Layout from "./Layout"
 import Home from "../modules/Home"
 import Page from "../modules/pages/Page"
+import Base from "./layout/Base"
 
 const Routing = () => {
 	return (
 		<BrowserRouter>
             <Routes>
 				<Route path="/" element={<Home />} />
-                <Route path="/page" element={<Layout />}>
+                <Route path="/page" element={<Base />}>
                     <Route path="1" element={<Page pageNumber="1" />} />
 					<Route path="2" element={<Page pageNumber="2" />} />
                 </Route>
